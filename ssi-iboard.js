179913af631f11a9953c4f8f9336cc215e2478f0
QuocHieuNguyen <50551410+QuocHieuNguyen@users.noreply.github.com>
2026-04-15 20:05:50 +0700

    Update ssi-iboard.js

--- ssi-iboard.js
+++ ssi-iboard.js
@@ -5,9 +5,9 @@ if (element) {
     // 2. Ghi đè giá trị của biến --tw-scale-x trực tiếp vào style của phần tử
     // Việc này sẽ ưu tiên cao hơn class .scale-100 trong CSS
     element.style.setProperty('--tw-scale-x', '1.07');
-    element.style.setProperty('--tw-scale-y', '1.4');
+    element.style.setProperty('--tw-scale-y', '1.5');
     console.log("Đã cập nhật --tw-scale-x thành 1.07");
-    console.log("Đã cập nhật --tw-scale-y thành 1.4");
+    console.log("Đã cập nhật --tw-scale-y thành 1.5");
 } else {
     console.log("Không tìm thấy phần tử có id 'stock-detail-modal'");
 }
